a2918efdd3f05efbc50a888aa9149897bb2bb457
repineda <repineda@ucsc.edu>
2019-03-03 14:50:53 -0800

    changed mainScript to rename and move files to wiki

--- mainScript.sh
+++ mainScript.sh
@@ -17,10 +17,16 @@ testAllAlgorithms() {
 }
 
 pushWiki() {
+	now="$(date +'%d_%m_%Y_%H:%M')"
+        echo $now
+        for _file in *.csv ; do
+                mv $_file "$now$_file"
+                echo $_file
+                mv  ./$now$_file ~/project-lava.wiki/Data/
+        done
 	cd ..
         cd project-lava.wiki
         git pull
-        mv  ~/project-lava/TestingEnvironment/Framework/output.csv ~/project-lava.wiki/Data/
         git add -A
         git commit -m "Adding run to the Database"
         git push	
