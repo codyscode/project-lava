36ece1b7e221a367fb327c82a5a654ff27e34c7c
repineda <44100673+repineda@users.noreply.github.com>
2019-03-02 18:50:21 -0800

    added sudo to the testScript
    This is needed since we assign threads to cores.

--- testScript.sh
+++ testScript.sh
@@ -12,7 +12,7 @@
 
 for ((i=1; $i < 9; i++)) ; do
 	for((j=1; $j < 9; j++)) ; do
-		./TestingEnvironment/Framework/framework $i $j
+		sudo ./TestingEnvironment/Framework/framework $i $j
 	done
 done
 
