48fea8eb7d63e297ab1fc8991641560bd7fea5fc
repineda <repineda@ucsc.edu>
2019-03-07 14:07:29 -0800

    Added new short test

--- testScript.sh
+++ testScript.sh
@@ -12,11 +12,16 @@
 
 #set -e
 #disabled for testing but needs to be turned back on for real runs.
+if [ $1 == "-q" ]
+	then 
+		./TestingEnvironment/Framework/framework 4 4
+	else
 
-for ((i=1; $i < 9; i++)) ; do
-	for((j=1; $j < 9; j++)) ; do
-		echo $i $j
-		./TestingEnvironment/Framework/framework $i $j
-	done
-done
 
+		for ((i=1; $i < 9; i++)) ; do
+			for((j=1; $j < 9; j++)) ; do
+				echo $i $j
+				./TestingEnvironment/Framework/framework $i $j
+			done
+		done
+fi
