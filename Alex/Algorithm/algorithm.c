4e10555f4fed753fcd19813ec8f179a479b80f8e
Alex Widmann <awidmann@ucsc.edu>
2019-05-11 12:33:18 -0700

    Updated comments in algorithm

--- Alex/Algorithm/algorithm.c
+++ Alex/Algorithm/algorithm.c
@@ -6,7 +6,8 @@ though each byte gets memcpyd twice as many times, it's significantly faster.
 Presumably because caches aren't thrashing.
 
 This builds off of algorithm 6 and segments the buffer into 2 separate buffers
-The loop is unraveled to allow fast indexing;
+The algorithm can be sped up by unraveling the for loop, however it is currently like
+this to allow testing of the optimal number of segments
 */
 
 #include "../FrameworkSRC/global.h"
@@ -18,6 +19,7 @@ The loop is unraveled to allow fast indexing;
 //adjusted. For example, buffers half this size were only 1 Gbps slower for 8 to 8.
 #define BUFFSIZEBYTES 65536
 
+//Number of segments for the queue
 #define NUM_SEGS 2
 
 typedef struct VBSegment{
@@ -45,8 +47,9 @@ function get_output_thread(){
 
 void initializeCustomQueues(){
     for (int i = 0; i < MAX_NUM_INPUT_THREADS; i++){
-        queues[i].segment[0].ptr = queues[i].segment[0].buffer;
-        queues[i].segment[1].ptr = queues[i].segment[1].buffer;
+        for(int j = 0; j < NUM_SEGS; j++){
+            queues[i].segment[j].ptr = queues[i].segment[j].buffer;
+        }
     }
 }
 
@@ -60,8 +63,7 @@ void * input_thread(void * args){
     size_t threadIndex = inputArgs->threadNum;
 
     //Each input thread is assigned a single shared queue
-    vbseg_t* shared1 = &queues[threadIndex].segment[0];
-    vbseg_t* shared2 = &queues[threadIndex].segment[1];
+    vbseg_t* shared1;
 
     //Initialize a local queue
     vbseg_t local;
@@ -88,89 +90,46 @@ void * input_thread(void * args){
     //is full the entire vector is copied to the shared buffer.
     while(1){
         for(size_t i = 0; i < NUM_SEGS; i++){
-            
-        }
-        // >>>>>>>>>>>>>>>>> SEGMENT 1 <<<<<<<<<<<<<<<<
-        while(1){
-            // *** START PACKET GENERATOR ***
-            //Min value: offset || Max value: offset + 7
-            seed0 = (214013 * seed0 + 2531011);   
-            currFlow = ((seed0 >> 16) & FLOWS_PER_THREAD_MOD) + offset;
+            shared1 = &queues[threadIndex].segment[i];
+            while(1){
+                // *** START PACKET GENERATOR ***
+                //Min value: offset || Max value: offset + 7
+                seed0 = (214013 * seed0 + 2531011);   
+                currFlow = ((seed0 >> 16) & FLOWS_PER_THREAD_MOD) + offset;
 
-            //Min value: 64 || Max value: 8191 + 64
-            seed1 = (214013 * seed1 + 2531011); 
-            currLength = ((seed1 >> 16) % (MAX_PAYLOAD_SIZE_MOD - MIN_PAYLOAD_SIZE)) + MIN_PAYLOAD_SIZE; 
-            // *** END PACKET GENERATOR  ***
+                //Min value: 64 || Max value: 8191 + 64
+                seed1 = (214013 * seed1 + 2531011); 
+                currLength = ((seed1 >> 16) % (MAX_PAYLOAD_SIZE_MOD - MIN_PAYLOAD_SIZE)) + MIN_PAYLOAD_SIZE; 
+                // *** END PACKET GENERATOR  ***
 
-            memcpy(local.ptr, &currFlow, 8);
-            local.ptr += 8;
-            memcpy(local.ptr, &currLength, 8);
-            local.ptr += 8;
-            memcpy(local.ptr, &orderForFlow[currFlow - offset], 8);
-            local.ptr += 8;
-            memcpy(local.ptr, data, currLength);
-            local.ptr += currLength;
+                memcpy(local.ptr, &currFlow, 8);
+                local.ptr += 8;
+                memcpy(local.ptr, &currLength, 8);
+                local.ptr += 8;
+                memcpy(local.ptr, &orderForFlow[currFlow - offset], 8);
+                local.ptr += 8;
+                memcpy(local.ptr, data, currLength);
+                local.ptr += currLength;
 
-            //Update the next flow number to assign
-            orderForFlow[currFlow - offset]++;
-            
-            //If we don't have room in the local buffer for another packet it's time to memcpy to shared memory.
-            //A timeout could be added for real-world situations where few packets are coming in and local buffers
-            //take a long time to fill.
-            if ((local.ptr - local.buffer + MAX_PACKET_SIZE) >= BUFFSIZEBYTES) {
-                //If there's still data in the shared buffer, wait
-                while (shared1->ptr > shared1->buffer) {
-                    ;
+                //Update the next flow number to assign
+                orderForFlow[currFlow - offset]++;
+                
+                //If we don't have room in the local buffer for another packet it's time to memcpy to shared memory.
+                //A timeout could be added for real-world situations where few packets are coming in and local buffers
+                //take a long time to fill.
+                if ((local.ptr - local.buffer + MAX_PACKET_SIZE) >= BUFFSIZEBYTES) {
+                    //If there's still data in the shared buffer, wait
+                    while (shared1->ptr > shared1->buffer) {
+                        ;
+                    }
+                    //Copy the entire vector to shared memory
+                    memcpy(shared1->buffer, local.buffer, (local.ptr - local.buffer));
+                    //Signal to output_thread there's more data in shared memory and how much
+                    shared1->ptr = shared1->buffer + (local.ptr - local.buffer);
+                    //Reset the local queue
+                    local.ptr = local.buffer;
+                    break;
                 }
-                //Copy the entire vector to shared memory
-                memcpy(shared1->buffer, local.buffer, (local.ptr - local.buffer));
-                //Signal to output_thread there's more data in shared memory and how much
-                shared1->ptr = shared1->buffer + (local.ptr - local.buffer);
-                //Reset the local queue
-                local.ptr = local.buffer;
-                break;
-            }
-        }
-        
-        // >>>>>>>>>>>>>>>>> SEGMENT 2 <<<<<<<<<<<<<<<<
-        while(1){
-            // *** START PACKET GENERATOR ***
-            //Min value: offset || Max value: offset + 7
-            seed0 = (214013 * seed0 + 2531011);   
-            currFlow = ((seed0 >> 16) & FLOWS_PER_THREAD_MOD) + offset;
-
-            //Min value: 64 || Max value: 8191 + 64
-            seed1 = (214013 * seed1 + 2531011); 
-            currLength = ((seed1 >> 16) % (MAX_PAYLOAD_SIZE_MOD - MIN_PAYLOAD_SIZE)) + MIN_PAYLOAD_SIZE; 
-            // *** END PACKET GENERATOR  ***
-
-            memcpy(local.ptr, &currFlow, 8);
-            local.ptr += 8;
-            memcpy(local.ptr, &currLength, 8);
-            local.ptr += 8;
-            memcpy(local.ptr, &orderForFlow[currFlow - offset], 8);
-            local.ptr += 8;
-            memcpy(local.ptr, data, currLength);
-            local.ptr += currLength;
-
-            //Update the next flow number to assign
-            orderForFlow[currFlow - offset]++;
-            
-            //If we don't have room in the local buffer for another packet it's time to memcpy to shared memory.
-            //A timeout could be added for real-world situations where few packets are coming in and local buffers
-            //take a long time to fill.
-            if ((local.ptr - local.buffer + MAX_PACKET_SIZE) >= BUFFSIZEBYTES) {
-                //If there's still data in the shared buffer, wait
-                while (shared2->ptr > shared2->buffer) {
-                    ;
-                }
-                //Copy the entire vector to shared memory
-                memcpy(shared2->buffer, local.buffer, (local.ptr - local.buffer));
-                //Signal to output_thread there's more data in shared memory and how much
-                shared2->ptr = shared2->buffer + (local.ptr - local.buffer);
-                //Reset the local queue
-                local.ptr = local.buffer;
-                break;
             }
         }
     }
@@ -187,8 +146,7 @@ void * output_thread(void * args){
     size_t qIndex = outputArgs->threadNum;
 
     //Start on the first shared queue this output thread is reponsible for
-    vbseg_t* shared1 = &queues[qIndex].segment[0];
-    vbseg_t* shared2 = &queues[qIndex].segment[1];
+    vbseg_t* shared1;
 
     //Local variable version of the global variables
     size_t numOutput = outputThreadCount;
@@ -214,124 +172,68 @@ void * output_thread(void * args){
 
     //Each iteration copies a full vector from shared memory and processes it.
     while(1){
-        // >>>>>>>>>>>>>>>>>>>>> SEGMENT 1 <<<<<<<<<<<<<<<<<<<<
-        //Wait until more data has been written to shared memory
-        while (shared1->ptr == shared1->buffer) {
-            ;
-        }
-        //Copy the entire vector from shared to local memory
-        memcpy(local.buffer, shared1->buffer, (shared1->ptr - shared1->buffer));
+        for(size_t i = 0; i < NUM_SEGS; i++){
+            shared1 = &queues[qIndex].segment[i];
+            // >>>>>>>>>>>>>>>>>>>>> SEGMENT 1 <<<<<<<<<<<<<<<<<<<<
+            //Wait until more data has been written to shared memory
+            while (shared1->ptr == shared1->buffer) {
+                ;
+            }
+            //Copy the entire vector from shared to local memory
+            memcpy(local.buffer, shared1->buffer, (shared1->ptr - shared1->buffer));
 
-        //local.ptr marks where data in the local buffer ends
-        local.ptr = local.buffer + (shared1->ptr - shared1->buffer);
+            //local.ptr marks where data in the local buffer ends
+            local.ptr = local.buffer + (shared1->ptr - shared1->buffer);
 
-        //Signal to input_thread that shared memory can be written to again
-        shared1->ptr = shared1->buffer;
+            //Signal to input_thread that shared memory can be written to again
+            shared1->ptr = shared1->buffer;
 
-        //readPtr points to the current packet in the local buffer
-        readPtr = local.buffer;
+            //readPtr points to the current packet in the local buffer
+            readPtr = local.buffer;
 
-        //Process all packets in the local buffer.
-        while (readPtr < local.ptr) {
-            //This second memcpy arguably isn't needed since all the packet data is local to this
-            //thread at this point but I didn't want there to be any confusion over whether this
-            //accurately models individual packets being parsed and found that adding it doesn't
-            //affect the speed.  
-            memcpy(&packet, readPtr, ((packet_t*) readPtr)->length + 24);
+            //Process all packets in the local buffer.
+            while (readPtr < local.ptr) {
+                //This second memcpy arguably isn't needed since all the packet data is local to this
+                //thread at this point but I didn't want there to be any confusion over whether this
+                //accurately models individual packets being parsed and found that adding it doesn't
+                //affect the speed.  
+                memcpy(&packet, readPtr, ((packet_t*) readPtr)->length + 24);
 
-            //Packets order must be equal to the expected order.
-            if(expected[packet.flow] != packet.order){
-                //Print out the contents of the local buffer that caused an error
-                int index = 0;
-                unsigned char* indexPtr = local.buffer;
-                while (indexPtr < local.ptr) {
-                    packet_t * errPacket = (packet_t*) indexPtr;
-                    printf("Position: %d, Flow: %ld, Order: %ld\n", index, errPacket->flow, errPacket->order);
-                    index++;
-                    indexPtr += (errPacket->length + 24);
+                //Packets order must be equal to the expected order.
+                if(expected[packet.flow] != packet.order){
+                    //Print out the contents of the local buffer that caused an error
+                    int index = 0;
+                    unsigned char* indexPtr = local.buffer;
+                    while (indexPtr < local.ptr) {
+                        packet_t * errPacket = (packet_t*) indexPtr;
+                        printf("Position: %d, Flow: %ld, Order: %ld\n", index, errPacket->flow, errPacket->order);
+                        index++;
+                        indexPtr += (errPacket->length + 24);
+                    }
+
+                    //Print out the specific packet that caused the error to the user
+                    printf("Error Packet: Flow %lu | Order %lu\n", packet.flow, packet.order);
+                    printf("Packet out of order in thread: %lu. Expected %lu | Got %lu\n", outputArgs->threadNum, expected[packet.flow], packet.order);
+                    exit(0);
                 }
+                else{              
+                    //Set what the next expected packet for the flow should be
+                    expected[packet.flow]++;
 
-                //Print out the specific packet that caused the error to the user
-                printf("Error Packet: Flow %lu | Order %lu\n", packet.flow, packet.order);
-                printf("Packet out of order in thread: %lu. Expected %lu | Got %lu\n", outputArgs->threadNum, expected[packet.flow], packet.order);
-                exit(0);
+                    //Move readPtr to address of next packet
+                    readPtr += (packet.length + 24);
+                }
             }
-            else{              
-                //Set what the next expected packet for the flow should be
-                expected[packet.flow]++;
 
-                //Move readPtr to address of next packet
-                readPtr += (packet.length + 24);
-            }
+            //At the end of this loop all packets in the local buffer have been processed and we update byteCount
+            output[outputArgs->threadNum].byteCount += (readPtr - local.buffer);
         }
-
-        //At the end of this loop all packets in the local buffer have been processed and we update byteCount
-        output[outputArgs->threadNum].byteCount += (readPtr - local.buffer);
 
         //Move to the next shared queue this output thread is responsible for
         qIndex = qIndex + numOutput;
         if(qIndex >= numInput) {
             qIndex = outputArgs->threadNum;
         }
-        shared1 = &queues[qIndex].segment[0];
-
-        // >>>>>>>>>>>>>>>>>>>>> SEGMENT 2 <<<<<<<<<<<<<<<<<<<<
-        //Move to the next segment
-        //Wait until more data has been written to shared memory
-        while (shared2->ptr == shared2->buffer) {
-            ;
-        }
-        //Copy the entire vector from shared to local memory
-        memcpy(local.buffer, shared2->buffer, (shared2->ptr - shared2->buffer));
-
-        //local.ptr marks where data in the local buffer ends
-        local.ptr = local.buffer + (shared2->ptr - shared2->buffer);
-
-        //Signal to input_thread that shared memory can be written to again
-        shared2->ptr = shared2->buffer;
-
-        //readPtr points to the current packet in the local buffer
-        readPtr = local.buffer;
-
-        //Process all packets in the local buffer.
-        while (readPtr < local.ptr) {
-            //This second memcpy arguably isn't needed since all the packet data is local to this
-            //thread at this point but I didn't want there to be any confusion over whether this
-            //accurately models individual packets being parsed and found that adding it doesn't
-            //affect the speed.  
-            memcpy(&packet, readPtr, ((packet_t*) readPtr)->length + 24);
-
-            //Packets order must be equal to the expected order.
-            if(expected[packet.flow] != packet.order){
-                //Print out the contents of the local buffer that caused an error
-                int index = 0;
-                unsigned char* indexPtr = local.buffer;
-                while (indexPtr < local.ptr) {
-                    packet_t * errPacket = (packet_t*) indexPtr;
-                    printf("Position: %d, Flow: %ld, Order: %ld\n", index, errPacket->flow, errPacket->order);
-                    index++;
-                    indexPtr += (errPacket->length + 24);
-                }
-
-                //Print out the specific packet that caused the error to the user
-                printf("Error Packet: Flow %lu | Order %lu\n", packet.flow, packet.order);
-                printf("Packet out of order in thread: %lu. Expected %lu | Got %lu\n", outputArgs->threadNum, expected[packet.flow], packet.order);
-                exit(0);
-            }
-            else{              
-                //Set what the next expected packet for the flow should be
-                expected[packet.flow]++;
-
-                //Move readPtr to address of next packet
-                readPtr += (packet.length + 24);
-            }
-        }
-
-        //At the end of this loop all packets in the local buffer have been processed and we update byteCount
-        output[outputArgs->threadNum].byteCount += (readPtr - local.buffer);
-
-        //Assign the second segment to the new queue's second segment
-        shared2 = &queues[qIndex].segment[1];
     }
 
     return NULL;
