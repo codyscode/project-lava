c149785bcfe71ae3256928e854312d534f27d6fa
repineda <repineda@ucsc.edu>
2019-06-03 05:23:35 -0700

    updated comments

--- TestingEnvironment/testScript.sh
+++ TestingEnvironment/testScript.sh
@@ -3,9 +3,12 @@ OPTION_VAL=""
 #running through each iteration of m input queues and n output queues each value up to 8.
 #set -e
 #disabled for testing but needs to be turned back on for real runs.
+
+#Runs only the 1:1 iteration of each algorithm in the TestingEnvironment
 quickTest(){
 	./framework 1 1 i
 }
+#runs through all iterations
 normalTest(){
 	for ((i=1; $i < 9; i++)) ; do
 			for((j=1; $j < 9; j++)) ; do
@@ -14,6 +17,7 @@ normalTest(){
 			done
 		done
 }
+#runs through a specfic iteration
 specificTest(){
 	count=0
 	for opt in $OPTION_VAL; do
@@ -30,6 +34,7 @@ specificTest(){
 	printf "\n>>>>>>>>>> INPUT: $inputT AND OUTPUT: $outputT <<<<<<<<<<<<\n"
 	./framework "$inputT" "$outputT" i
 }
+#Handles the arguments
 while getopts 'qns:' flag; do
 	case "${flag}" in
 		q) quickTest ;;
