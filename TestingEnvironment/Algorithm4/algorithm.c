141ba2d8a83daaf74e0857d834dcb506a8548ec6
codyscode <codyhc520@gmail.com>
2019-06-03 17:05:00 -0700

    Removed commented out code

--- TestingEnvironment/Algorithm4/algorithm.c
+++ TestingEnvironment/Algorithm4/algorithm.c
@@ -23,19 +23,12 @@ function get_output_thread(){
     return output_thread;
 }
 
-//The job of the input threads is to make packets to populate the buffers. As of now the packets are stored in a buffer.
-//Attributes:
-// -Each input queue generates packets in a circular buffer
-// -Input queue stops writing when the buffer is full and continues when it is empty
-// -Each input queue generates 5 flows (i.e input 1: 1, 2, 3, 4, 5; input 2: 6, 7, 8, 9, 10)
-// -The flows can be scrambled coming from a single input (i.e stream: 1, 2, 1, 3, 4, 4, 3, 3, 5)
 void * input_thread(void * args){
-//Get arguments and any other functions for input threads
+    //Get arguments for input threads
     threadArgs_t *inputArgs = (threadArgs_t *)args;
 
     size_t threadNum = inputArgs->threadNum;   
     queue_t * inputQueue = &input[inputArgs->threadNum].queue;
-
     
     //Set the thread to its own core
     set_thread_props(inputArgs->coreNum, 2);
@@ -45,8 +38,6 @@ void * input_thread(void * args){
     size_t currFlow, currLength;
     size_t offset = threadNum * FLOWS_PER_THREAD;
 	
-    //Continuously generate input numbers until the buffer fills up. 
-    //Once it hits an entry that is not empty, it will wait until the input is grabbed.
     size_t index = 0;
     
     register unsigned int seed0 = (unsigned int)time(NULL);
@@ -57,7 +48,6 @@ void * input_thread(void * args){
     //Wait until everything else is ready
     while(startFlag == 0);
 
-    //After having the base queue fill up, maintain all the packets in the queue by just overwriting their order number
     while(1){
         // *** START PACKET GENERATOR ***
         //Min value: offset || Max value: offset + 7
@@ -71,19 +61,14 @@ void * input_thread(void * args){
 
         //If the queue spot is filled then that means the input buffer is full so continuously check until it becomes open
         while((*inputQueue).data[index].isOccupied == OCCUPIED){
-            ;//Do Nothing until a space is available to write
+            ;
         }
 
         packet_t packet;
         packet.order = orderForFlow[currFlow - offset];
         packet.length = currLength;
         packet.flow = currFlow;
-/*
-        //Create the new packet. Write the order and length first before flow as flow > 0 indicates theres a packet there
-        (*inputQueue).data[index].packet.order = orderForFlow[currFlow - offset];
-        (*inputQueue).data[index].packet.length = currLength;
-        (*inputQueue).data[index].packet.flow = currFlow;
-*/        
+   
         //memcpy simulates the packets data actually being written into the queue by the input thread
         memcpy(&(*inputQueue).data[index].packet, &packet, currLength + PACKET_HEADER_SIZE);
 
@@ -100,9 +85,7 @@ void * input_thread(void * args){
     return NULL;
 }
 
-//This is the old output processing thread from the framework
-//The job of the processing threads is to ensure that the packets are being delivered in proper order by going through
-//output queues and reading the order
+
 void * output_thread(void * args){
     //Get arguments and any other functions for input threads
     threadArgs_t *outputArgs = (threadArgs_t *)args;
@@ -117,8 +100,7 @@ void * output_thread(void * args){
     //Set the thread to its own core
     set_thread_props(outputArgs->coreNum, 2);
 
-    //"Process" packets to confirm they are in the correct order before consuming more. 
-    //Processing threads process until they get to a spot with no packets
+    //Verifies order for a given flow
     size_t expected[MAX_NUM_INPUT_THREADS * FLOWS_PER_THREAD] = {0}; 
     size_t index; 
 
@@ -127,15 +109,9 @@ void * output_thread(void * args){
     //Wait until everything else is ready
     while(startFlag == 0);
 
-    //Go through each space in the output queue until we reach an emtpy space in which case we swap to the other queue to process its packets
     while(1){
         index = (*outputQueue).toRead;
-/*
-        //If there is no packet, continuously check until something shows up
-        while((*outputQueue).data[index].isOccupied == NOT_OCCUPIED){
-            ;//Do Nothing until there is a packet to read
-        }
-*/
+
         if ((*outputQueue).data[index].isOccupied == NOT_OCCUPIED) {
             //Move to the next queue this output thread is responsible for
             currentQueue = currentQueue + outputThreadCount;
@@ -154,8 +130,6 @@ void * output_thread(void * args){
         }
 		
         //Packets order must be equal to the expected order.
-        //Implementing less than currflow causes race conditions with writing
-        //Any line that starts with a * is ignored by python script
         if(expected[currFlow] != (*outputQueue).data[index].packet.order){
             //Print out the contents of the processing queue that caused an error
             for(int i = 0; i < BUFFERSIZE; i++){
@@ -167,7 +141,6 @@ void * output_thread(void * args){
             printf("Packet out of order in Output Queue %lu. Expected %lu | Got %lu\n", threadNum, expected[currFlow], (*outputQueue).data[index].packet.order);
             exit(0);
         }
-        //Else "process" the packet by filling in 0 and incrementing the next expected
         else{            
             
             //Set what the next expected packet for the flow should be
@@ -178,26 +151,16 @@ void * output_thread(void * args){
             (*outputQueue).toRead = (*outputQueue).toRead % BUFFERSIZE;
 	 
             //memcpy simulates the packets data being processed by the output thread.
-	    memcpy(dummyDestination, (*outputQueue).data[index].packet.payload, (*outputQueue).data[index].packet.length);
-
+	        memcpy(dummyDestination, (*outputQueue).data[index].packet.payload, (*outputQueue).data[index].packet.length);
 
             //increment the number of bits passed
             output[threadNum].byteCount += (*outputQueue).data[index].packet.length + PACKET_HEADER_SIZE;
             //Set the position to free. Say it has already processed data
             (*outputQueue).data[index].isOccupied = NOT_OCCUPIED;
-/*
-            //Move to the next queue this output thread is responsible for
-            currentQueue = currentQueue + outputThreadCount;
-            if(currentQueue >= inputThreadCount) {
-                currentQueue = threadNum;
-            }
-            outputQueue = &(input[currentQueue].queue);
-*/
         }
     }
     return NULL;
 }
-
 
 pthread_t * run(void *argsv){
 
